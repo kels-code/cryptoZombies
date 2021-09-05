b8d307d072fd70a6a5e98de19023d057726c1cea
Kelvin W <kelvinjw26@gmail.com>
2021-09-04 23:40:39 -0500

    add comments

--- lesson-2/zombiefactory.sol
+++ lesson-2/zombiefactory.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.6.0;
 contract ZombieFactory {
 
     event NewZombie(uint zombieId, string name, uint dna);
-
+    // DNA sequence character limit
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -13,23 +13,31 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
-
+    // Map zombie's ID with the user's address
     mapping (uint => address) public zombieToOwner;
+    // Map the user's address to the zombie count
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) private {
+        // Create zombie ID using 'zombies' array index
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // At index ID, map the value of Message Sender
         zombieToOwner[id] = msg.sender;
+        // Add 1 to the owner's zombie count
         ownerZombieCount[msg.sender]++;
+        // Trigger event of new zombie creation
         emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
+        // Create random dna of zombie with keccak256 hash
         uint rand = uint(keccak256(abi.encodePacked(_str)));
+        // Be sure zombie DNA length is equal to dnaDigits
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string memory _name) public {
+        // Require that the Message Sender has no zombies before easily creating random ones
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
--- lesson-2/zombiefeeding.sol
+++ lesson-2/zombiefeeding.sol
@@ -5,7 +5,9 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    // Verify only owner of zombie can feed said zombie
     require(msg.sender == zombieToOwner[_zombieId]);
+    // Get a hold of this new zombies DNA
     Zombie storage myZombie = zombies[_zombieId];
   }
 
